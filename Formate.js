b4759027c8e6cf8f32040c2411feb37a6e100849
Ravi <96724856+Amit4581@users.noreply.github.com>
2023-11-22 14:28:51 +0530

    Add files via upload
    date formate

---
+++ Formate.js
@@ -0,0 +1,11 @@
+function formatDate(inputDate) {
+    // Parse the input date string
+    var dateParts = inputDate.split('-');
+    var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var monthIndex = monthNames.indexOf(dateParts[1]);
+    var formattedDate = new Date(dateParts[2], monthIndex, dateParts[0]);
+    var yyyy = formattedDate.getFullYear();
+    var mm = String(formattedDate.getMonth() + 1).padStart(2, '0'); // January is 0!
+    var dd = String(formattedDate.getDate()).padStart(2, '0');
+    return yyyy + '-' + mm + '-' + dd;
+}
